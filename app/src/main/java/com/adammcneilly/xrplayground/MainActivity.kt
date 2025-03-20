7d683c18ef7483aec4bb610405952d6577b7d4c9
Adam McNeilly <amcneilly331@gmail.com>
2025-03-19 20:05:13 -0400

    Creating an AR Core session.

--- app/src/main/java/com/adammcneilly/xrplayground/MainActivity.kt
+++ app/src/main/java/com/adammcneilly/xrplayground/MainActivity.kt
@@ -4,15 +4,35 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.xr.runtime.Session
+import androidx.xr.runtime.SessionCreatePermissionsNotGranted
+import androidx.xr.runtime.SessionCreateSuccess
 import com.adammcneilly.xrplayground.theme.XRTheme
 
 class MainActivity : ComponentActivity() {
+    private val permissionRequestLauncher = registerForActivityResult(
+        ActivityResultContracts.RequestMultiplePermissions(),
+    ) { grantedPermissions ->
+        val allPermissionsGranted = grantedPermissions.all { entry ->
+            entry.value
+        }
+
+        if (allPermissionsGranted) {
+            createSession()
+        }
+    }
+
+    private var session: Session? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        createSession()
 
         setContent {
             enableEdgeToEdge()
@@ -23,6 +43,20 @@ class MainActivity : ComponentActivity() {
                 ) {
                     Greeting("Android")
                 }
+            }
+        }
+    }
+
+    private fun createSession() {
+        when (val sessionResult = Session.create(this)) {
+            is SessionCreateSuccess -> {
+                // Handle success
+                session = sessionResult.session
+            }
+
+            is SessionCreatePermissionsNotGranted -> {
+                // Request permissions
+                permissionRequestLauncher.launch(sessionResult.permissions.toTypedArray())
             }
         }
     }
